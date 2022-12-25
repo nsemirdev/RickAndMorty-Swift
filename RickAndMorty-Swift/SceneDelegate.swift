b5c78190b9d247e504d0caf7cb76eeb54b43d3c2
nsemirdev <nsemirdev@gmail.com>
2022-12-26 00:02:45 +0300

    no storyboard initial setup

---
+++ RickAndMorty-Swift/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  RickAndMorty-Swift
+//
+//  Created by Emir Alkal on 26.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        
+        window?.rootViewController = ViewController()
+    }
+
+}
+
---
+++ RickAndMorty-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty-Swift
+//
+//  Created by Emir Alkal on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
